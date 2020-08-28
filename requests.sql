2388373e0d541cd93e89b00aeed6b7880998c172
AntonyVasilev <antony.n.vasilev@gmail.com>
2020-08-28 09:48:30 +0300

    update requests

--- requests.sql
+++ requests.sql
@@ -29,10 +29,19 @@ where user_id in
 
 -- Вывести информацию об отелях в указанном городе
 
+set @city = 'Port Rubiemouth';
 select 
+	id as hotel_id,
 	property_name as name,
-	'property type',
-	'country',
-	'city',
+	(select name from property_types where id = (select property_type_id from properties where id = hotel_id)) as 'property type',
+	(select name from countries where id = (select country_id from properties where id = hotel_id)) as country,
+	(select name from cities where id = (select city_id from properties where id = hotel_id)) as city,
 	address,
-	rating
+	rating,
+	(select room_count from property_profiles where property_id = hotel_id) as 'room count',
+	(select property_facilities from property_profiles where property_id = hotel_id) as 'property facilities',
+	(select languages_spoken from property_profiles where property_id = hotel_id) as 'languages spoken',
+	(select bank_cards_allowed from property_profiles where property_id = hotel_id) as 'bank cards allowed',
+	(select meals from property_profiles where property_id = hotel_id) as 'meals'
+from properties
+where city_id in (select id from cities where name = @city);
