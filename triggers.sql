6c6c3f22387ca6d7669d04bd9080fc19d4446ea8
Antony Vasilev <antony.n.vasilev@gmail.com>
2020-09-09 21:47:54 +0300

    add one more trigger

--- triggers.sql
+++ triggers.sql
@@ -45,3 +45,45 @@ delimiter ;
 
 insert into bookings (user_id, property_id, room_type_id, room_count, booked_from_date, booked_till_date, book_type)
 values ('5', '8', '1', '1', '2020-08-15', '2020-08-20', 'requested');
+
+set @insert_id = LAST_INSERT_ID(); 
+
+update bookings 
+set booked_from_date = '2020-11-15', 
+	booked_till_date = '2020-11-20'
+where id = @insert_id;
+
+update bookings 
+set booked_from_date = '2020-08-15', 
+	booked_till_date = '2020-08-20'
+where id = @insert_id;
+
+
+-- Тригер, запрещающий изменение города и/или адреса нахождения объекта размещения
+
+drop trigger if exists check_update_property_city_address;
+
+delimiter //
+
+create trigger check_update_property_city_address before update on booking.properties
+for each row
+begin
+	if NEW.city_id <> OLD.city_id or NEW.address <> OLD.address then
+		SIGNAL SQLSTATE '45000' SET message_text = 'Update canceled. You can\'t change property location!';
+	end if;
+end//
+
+delimiter ;
+
+update properties 
+set city_id = '5'
+where id = '1';
+
+update properties 
+set address = '56794 Marianne Squares Suite 186 Sarinatown, WY 23946'
+where id = '3';
+
+update properties 
+set address = '56794 Marianne Squares Suite 186 Sarinatown, WY 23946',
+	city_id = '5'
+where id = '3';
